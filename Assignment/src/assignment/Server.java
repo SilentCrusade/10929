83ddb95296d094b80df449c204e12dddc7b69d0f
casiusclay <40061443+casiusclay@users.noreply.github.com>
2018-06-08 19:53:58 +0800

    Update Server.java

--- Assignment/src/assignment/Server.java
+++ Assignment/src/assignment/Server.java
@@ -1,32 +1,22 @@
 package assignment;
 
 import java.util.ArrayList;
-    
-public class Server{
-   
-    ArrayList<Caller> Server = new ArrayList<>();
-    
+
+public class Server {
+    ArrayList<Caller> Server = new ArrayList();
+
     public Server() {
-       
     }
-   
-    
+
     public void setAddCall(Caller Caller) {
-       
-       Server.add(Caller);
-               
+        this.Server.add(Caller);
     }
-    
-    public void setRemoveCall(){
-        
-        Server.remove(0);
-        
+
+    public void setRemoveCall() {
+        this.Server.remove(0);
     }
-    
-    public Caller getCall(){
-        
-        return Server.get(0);
-                
+
+    public Caller getCall() {
+        return (Caller)this.Server.get(0);
     }
-   
 }
