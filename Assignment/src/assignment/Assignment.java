6b8568186a4607ff43a8a02b5b8a47c5a6477917
casiusclay <40061443+casiusclay@users.noreply.github.com>
2018-06-10 00:50:25 +0800

    Updated with Basic Functionalities. - J.

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -1,192 +1,287 @@
 package assignment;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Assignment extends TimerTask {
-    ArrayList<Server> serverList = new ArrayList();
-    ArrayList<Caller> queue = new ArrayList();
+    ArrayList<Server> SList = new ArrayList<>(); //Server List
+    ArrayList<Caller> Q = new ArrayList<>(); //Queue List
     Random rand = new Random();
-    public static int amount = 0;
-    int time;
-    boolean initial;
-    public static int calls_processed = 0, calls_count = 0, first_attempt = 0, second_attempt = 0, third_attempt = 0, attempt;
+    
+    public static int amount; //Amount of servers to be inputted by user
+    int time = rand.nextInt(301) /*+ 300*/; //Random duration of program execution
+    boolean initial = false; //For servers to only be instantiated once
+    public static int calls_processed = 0, calls_count = 0, first_attempt = 0, second_attempt = 0, third_attempt = 0, lots_of_attempts = 0, attempt;
 
 
-    public Assignment() {
-        this.time = this.rand.nextInt(301) /*+ 300*/;
-        this.initial = false;
-    }
-
-    public static void main(String[] args) {
-        boolean valid = false;
+    public static void main(String[] args)  {
+        boolean valid = false; //To be used for user validation
+        
         Scanner sc = new Scanner(System.in);
         TimerTask task = new Assignment();
         Timer timer = new Timer();
-
-        while(!valid) {
-            System.out.print("Please specify the number of servers (1 - 99): ");
-
+        
+        while (!valid){
+            System.out.print("Please specify the number of servers (1 - 99): "); //Prompts user to enter a value
+            
             try {
-                amount = sc.nextInt();
-            } catch (Exception var6) {
-                System.out.println("Invalid input. Try again.");
+                amount = sc.nextInt(); //Accepts user input
+            }
+            catch (Exception e){ //If user enters a value other than integers, a message is displayed
+                System.out.println("Invalid input. Try again.\n");
                 sc.next();
                 continue;
             }
-
-            if (amount < 100 && amount > 0) {
+            
+            if ((amount < 100)&&(amount > 0)) //If user enters a value between 1 and 99, input is valid.
                 valid = true;
-            } else {
-                System.out.println("Invalid amount of servers. Try again.");
-            }
+            
+            else
+                System.out.println("Invalid amount of servers. Try again.\n");
         }
-
-        timer.scheduleAtFixedRate(task, 0L, 100L);
+        System.out.println();
+        
+        timer.scheduleAtFixedRate(task, 0, 100); //Auto-runs program every 1000 millisecond or 1 second
+       
     }
-
-    public void ServerManagement() {
-        if (!this.initial) {
-            for(int k = 0; k < amount; ++k) {
+    
+    public void ServerNo(){
+        if(!initial){
+            
+            for(int counter = 0; counter < amount; counter++){ //Makes instances of servers
                 Server s = new Server();
-                this.serverList.add(s);
+                SList.add(s);
             }
-
-            this.initial = true;
+            initial = true;
         }
-
+        
     }
-
+    
+    
+    @Override
     public void run() {
-        this.ServerManagement();
-        this.Time();
-        this.Generator();
-        calls_count++;
-        for(int n = 0; n < this.serverList.size(); ++n) {
-            if (!((Server)this.serverList.get(n)).Server.isEmpty()) {
-                if (((Server)this.serverList.get(n)).getCall().getTimeSlice() > 0) {
-                    if (((Server)this.serverList.get(n)).getCall().getServTime() > 1) {
-                        ((Server)this.serverList.get(n)).getCall().setServTime();
-                        ((Server)this.serverList.get(n)).getCall().runTimeSlice();
-                    } else {
-                        ((Server)this.serverList.get(n)).getCall().setAttempts();      //<-- added by J.
-                        ((Server)this.serverList.get(n)).setRemoveCall();
-                        if (this.queue.size() > 0) {
-                            ((Server)this.serverList.get(n)).setAddCall((Caller)this.queue.get(0));
-                            calls_processed++;      //<-- added by J.
-                            this.queue.remove(0);
+        
+        ServerNo(); //Instantiate servers
+        
+        Time(); //Duration program is allowed to run
+        
+        System.out.println("Event Log:");
+        
+        Arrival(); //Arrival of calls
+
+        calls_count++;  //call count is incremented as a call arrives/generated.
+
+        for(int counter = 0; counter < SList.size(); counter++){
+            //Check if Server is empty
+            if (!SList.get(counter).Server.isEmpty()){
+
+                //Subtract Service Time for a max of 7 seconds
+                if (SList.get(counter).getCall().getTimeSlice() > 0){
+
+                    //Check if there is remainder Service Time
+                    if (SList.get(counter).getCall().getServTime() > 1){
+
+                        SList.get(counter).getCall().setServTime(); //Decrements service time of a call by 1
+                        SList.get(counter).getCall().runTimeSlice(); //Decrements the time slice of a call by 1
+
+                    }
+
+                    //Remove Call from server when Service Time ends and retrieve new call
+                    else{
+                        
+                        System.out.println("ID " + SList.get(counter).getCall() + " has completed in Server " + (counter+1) + ".");
+
+                        SList.get(counter).getCall().setAttempts(); //the number of attempts for call is incremented as it has attempted to undergo processing.
+                        attempt = (SList.get(counter)).getCall().getAttempts();
+                        AttemptLog();   //logs call's data regarding how many attempts it made or/and how many times it had to be requeued.
+
+                        SList.get(counter).setRemoveCall(); //Removes a call from the server
+                        SList.get(counter).incCallCount(); //Increments the amount of calls processed by each server by 1
+                        
+                        if(Q.size() > 0){
+                            SList.get(counter).setAddCall(Q.get(0)); //Adds a call from queue if there is one
+                            Q.remove(0); //Removes a call from the front of the queue
+                            System.out.println("ID " + SList.get(counter).getCall() + " is assigned to Server " + (counter+1) + ".");
                         }
+                        
                     }
-                } else {
-                    attempt = ((Server)this.serverList.get(n)).getCall().getAttempts();
-                    this.queue.add(((Server)this.serverList.get(n)).getCall());
-                    ((Server)this.serverList.get(n)).setRemoveCall();
-                    if(attempt == 1){
-                        first_attempt++;
-                    }
-                    else if(attempt == 2){
-                        second_attempt++;
-                    }
-                    else if(attempt == 3){
-                        third_attempt++;
-                    }
+                    
+                    //Remove call from Server and place in queue after 7 seconds of call time slice
+                    if (!SList.get(counter).Server.isEmpty()){
+                        
+                        if (SList.get(counter).getCall().getTimeSlice() == 0){
+                            System.out.println("Time slice of ID " + SList.get(counter).getCall() + " in Server " + (counter+1) + " expired.");
 
-                    if (this.queue.size() > 0) {
-                        ((Server)this.serverList.get(n)).setAddCall((Caller)this.queue.get(0));
-                        calls_processed++;
-                        this.queue.remove(0);
-                    }
+                            SList.get(counter).getCall().setAttempts(); //the number of attempts for call is incremented as it has attempted to undergo processing.
+                            SList.get(counter).getCall().resetTimeSlice(); //Resets time slice of the call
+                            //attempt += (SList.get(counter)).getCall().getAttempts();
+                            Q.add(SList.get(counter).getCall()); //Adds current call to the end of the queue
+                            SList.get(counter).setRemoveCall(); //Removes call from server
 
-                    ((Server)this.serverList.get(n)).getCall().resetTimeSlice();
+                            if(Q.size() > 0){
+                                SList.get(counter).setAddCall(Q.get(0));
+                                Q.remove(0);
+                                System.out.println("ID " + SList.get(counter).getCall() + " is assigned to Server " + (counter+1) + ".");
+                            }
+                        }
+                        
+                    }
                 }
-            } else if (this.queue.size() > 0) {
-                ((Server)this.serverList.get(n)).setAddCall((Caller)this.queue.get(0));
-                this.queue.remove(0);
-                ((Server)this.serverList.get(n)).getCall().setAttempts();
-                calls_processed++;
+                
             }
+
+            //Add call from Queue if Server is empty 
+            else if (Q.size() > 0) {
+
+                SList.get(counter).setAddCall(Q.get(0));
+                Q.remove(0);
+                System.out.println("ID " + SList.get(counter).getCall() + " is assigned to Server " + (counter+1) + ".");
+                
+            }
+
         }
 
-        this.Output();
+        //Print output every interval (1 sec)
+        Printout();
+        
     }
-
-    public void Generator() {
-        Caller c = new Caller();
-        c.setCallID();
-        c.setRServTime();
-        c.resetTimeSlice();
-        int chance = this.rand.nextInt(3);
-        if (chance == 1) {
-            int m = 0;
-            if (m < this.serverList.size()) {
-                if (!((Server)this.serverList.get(m)).Server.isEmpty()) {
-                    this.queue.add(c);
-                } else {
-                    ((Server)this.serverList.get(m)).setAddCall(c);
-                }
-            }
+    
+    public void Arrival(){
+        
+        int chance;
+       
+        //Arrives call at an average of 20 calls/min
+        chance = rand.nextInt(3);
+       
+        if (chance == 1){
+            
+            Caller c = new Caller(); //Instantiates a caller
+      
+            //Set random CallID and Service Time via Caller class
+            c.setCallID();
+            c.setRServTime();
+            c.resetTimeSlice();
+            
+            System.out.println("ID " + c.getCallID() + " has been added to queue.");
+            Q.add(c); //Adds call to queue
+        
         }
-
+        
     }
+    
+    public void Time(){
+       
+        //Counts down duration of program
+        if (time != 0){
+        
+            time--;
+            
+        }
+        else{
+            //Exits program if time = 0
+            System.out.println("The program has ran out of time to run.");
+            
+            System.out.println("Total calls proceessed by");
+            for(int counter = 0; counter < SList.size(); counter++){
+                System.out.println("Server " + (counter+1) + ": " + SList.get(counter).getCallCount());
+                calls_processed += SList.get(counter).getCallCount();
+            }
 
-    public void Time() {
-        if (this.time != 0) {
-            --this.time;
-        } else {
-            System.out.println(":( The program has ran out of time to run");
             Details();
+            
             System.exit(0);
+            
         }
-
+        
     }
-
-    public void Output() {
+    
+    public void Printout (){
+       
         int x = 0;
-        System.out.println("\nTime Remaining: " + this.time + " seconds");
-        System.out.println("----------------------------------");
-        System.out.println("|   Name    |  ID  | Time Left(s) |");
+        String ServC, ServT;
+       
+        
+        //Prints time, Call ID and Service Time that is being processed
+        System.out.println("-----------------------------------");
+        System.out.println("|   Time Remaining: " + String.format("%03d", time) + " seconds   |");
+        System.out.println("-----------------------------------");
+        System.out.println("|  Server   |  ID  | Time Left(s) |");
+        System.out.println("-----------------------------------");
+        
+        for(int counter = 0; counter < SList.size(); counter++){
+            if (SList.get(counter).Server.size() > 0){
 
-        for(int i = 0; i < this.serverList.size(); ++i) {
-            String ServC;
-            String ServT;
-            if (((Server)this.serverList.get(i)).Server.size() > 0) {
-                ServC = ((Server)this.serverList.get(i)).getCall().toString();
-                ServT = String.format("%02d", ((Server)this.serverList.get(i)).getCall().getServTime());
-            } else {
+                //Assigns variables to current Call ID and corresponding Service Time
+                ServC = SList.get(counter).getCall().toString(); //CallID
+                ServT = String.format("%02d", SList.get(counter).getCall().getServTime());
+
+            }
+
+            else{
+                //Show nothing if there is no Call in Server
                 ServC = "    ";
-                ServT = "  ";
+                ServT= "  ";
+
             }
-
-            System.out.println("| Server " + String.format("%02d", i + 1) + " | " + ServC + " |      " + ServT + "      |");
+                
+            System.out.println("| Server " + String.format("%02d", counter+1) + " | " + ServC + " |      " + ServT + "      |");
+                
         }
-
-        System.out.println("----------------------------------");
+        
+        
+        System.out.println("-----------------------------------");
         System.out.println("Queue: ");
-        if (this.queue.size() > 0) {
-            while(x < this.queue.size()) {
-                System.out.println(this.queue.get(x) + " | " + ((Caller)this.queue.get(x)).getServTime());
-                ++x;
-            }
-        } else {
-            System.out.println("Empty");
-        }
+        
+        //Print calls present in the Queue
+        if (Q.size() > 0){
+           
+            while(x < Q.size()){
 
+                System.out.println(Q.get(x) + " | " + String.format("%02d", Q.get(x).getServTime()) + "s");
+                x++;
+
+            }
+            System.out.println();
+        }
+        
+        else
+            System.out.println("Empty\n");
     }
 
+    //Searches and logs the number of attempts or/and the number of times the calls had to be requeued.
+    public void AttemptLog() {
+        if(attempt == 1){
+            first_attempt++;    //if call took only one attempt then the number of calls
+            // that managed to process in one attempt (var first_attempt) will be incremented.
+        }
+        else if(attempt == 2){
+            second_attempt++;   //if call took two attempts then the number of calls
+            // that managed to process in two attempts after being enqueued once (var second_attempt) will be incremented.
+        }
+        else if(attempt == 3){
+            third_attempt++;    //if call took three attempts then the number of calls
+            // that managed to process in three attempts after being enqueued twice (var third_attempt) will be incremented.
+        }
+        else if (attempt > 3){
+            lots_of_attempts++; //if call took more than three attempts then the number of calls
+            // that managed to process in more than three attempts after being enqueued more than twice (var lots_of_attempts) will be incremented.
+        }
+    }
+
+    //prints details of the basic functionalities of the system.
     public void Details() {
         if (time == 0) {
-            System.out.print("------------------------------------------------------------------");
-            System.out.println("\nDetails ");
-            System.out.println("The total number of calls processed: " + calls_processed);
+            System.out.println("\n------------------------------------------------------------------");
+            System.out.println("Full Details");
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("The total number of calls processed by the system: " + calls_processed);
             System.out.println("Average number of calls processed per minute: " + (calls_processed / 60.0));
             System.out.println("The average arrival rate per minute: " + (double) (calls_count / 60));
             System.out.println("The number of calls processed on the first attempt (one time): " + first_attempt);
             System.out.println("The number of calls had to be requeued once (require 2 times) " + second_attempt);
             System.out.println("The number of calls had to be requeued twice (3 times): " + third_attempt);
+            System.out.println("The number of calls had to be requeued more than twice (>3 times): " + lots_of_attempts);
             System.out.print("------------------------------------------------------------------");
         }
     }
+    
 }
